1ca8a1539314cb8d85b88f2d13d8a679f03e34cc
Dima <benensondimitrii@gmail.com>
2022-03-25 20:07:29 +0300

    added some logic for changing rhythm

--- music-player/src/components/AudioPlayer.js
+++ music-player/src/components/AudioPlayer.js
@@ -13,11 +13,25 @@ const AudioPlayer = () => {
       audioPlayer.current.pause();
     }
   };
+  const togglePlaySpeedDown = () => {
+    audioPlayer.current.playbackRate = "-1"
+  };
+  const togglePlaySpeedUp = () => {
+    console.log(audioPlayer.current.playbackRate);
+    if (audioPlayer.current.playbackRate === 1) {
+      audioPlayer.current.playbackRate = 2;
+    } else if (audioPlayer.current.playbackRate === 2) {
+      audioPlayer.current.playbackRate = 3;
+    } else if (audioPlayer.current.playbackRate === 3) {
+      audioPlayer.current.playbackRate = 4;
+    } else if (audioPlayer.current.playbackRate === 4) {
+      audioPlayer.current.playbackRate = 5;
+    }
+  };
 
   return (
     <div>
       <p>Audio Player</p>
-
       <audio ref={audioPlayer} controls>
         <source
           src="https://www2.cs.uic.edu/~i101/SoundFiles/StarWars60.wav"
@@ -27,9 +41,13 @@ const AudioPlayer = () => {
       </audio>
 
       <div>
-        <button className="button-rhythm">J</button>
-        <button onClick={togglePlayPause} className="button-rhythm">K</button>
-        <button className="button-rhythm">L</button>
+        <button onClick={togglePlaySpeedDown} className="button-rhythm">J</button>
+        <button onClick={togglePlayPause} className="button-rhythm">
+          K
+        </button>
+        <button onClick={togglePlaySpeedUp} className="button-rhythm">
+          L
+        </button>
       </div>
     </div>
   );
