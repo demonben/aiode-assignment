d5750435fe6ff42aea09133e007f27502bd48094
Dima <benensondimitrii@gmail.com>
2022-03-25 15:14:20 +0300

    added play/stop logic and audio

--- music-player/src/components/AudioPlayer.js
+++ music-player/src/components/AudioPlayer.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const AudioPlayer = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const audioPlayer = useRef();
+
+  const togglePlayPause = () => {
+    setIsPlaying(!isPlaying);
+    if (isPlaying) {
+      audioPlayer.current.play();
+    } else {
+      audioPlayer.current.pause();
+    }
+  };
+
   return (
     <div>
       <p>Audio Player</p>
 
-      <audio controls>
-        <source src="../../music/file_example_WAV_1MG.wav" type="audio/wav" />
+      <audio ref={audioPlayer} controls>
+        <source
+          src="https://www2.cs.uic.edu/~i101/SoundFiles/StarWars60.wav"
+          type="audio/wav"
+        />
         Your browser does not support the audio element.
       </audio>
 
       <div>
         <button className="button-rhythm">J</button>
-        <button className="button-rhythm">K</button>
+        <button onClick={togglePlayPause} className="button-rhythm">K</button>
         <button className="button-rhythm">L</button>
       </div>
     </div>
