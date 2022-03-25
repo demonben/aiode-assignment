818e6d5bcaefd58a46c111c732605ebc08014ff9
Dima <benensondimitrii@gmail.com>
2022-03-25 13:58:23 +0300

    added buttons

--- music-player/src/App.js
+++ music-player/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>hello world</p>
+      <p>Audio Player</p>
+
+      <div>
+        <button className="button-rhythm">J</button>
+        <button className="button-rhythm">K</button>
+        <button className="button-rhythm">L</button>
+      </div>
     </div>
   );
 }
